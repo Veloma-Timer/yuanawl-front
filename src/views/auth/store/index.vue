8f8700953ff6133031ed3311e424fdeb3b0aec41
liuwei <lvyx620@163.com>
2023-05-31 13:41:53 +0800

    feat(component): 🚀 门店管理切面

--- src/views/auth/store/index.vue
+++ src/views/auth/store/index.vue
@@ -1,7 +1,66 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>store</div>
+  <div class="table-box">
+    <ProTable ref="proTable" title="门店管理" :columns="columns" :request-api="getTableList" :init-param="initParam">
+      <!-- 表格操作 -->
+      <template #operation="scope">
+        <el-button type="primary" link :icon="EditPen" @click="openDrawer('编辑', scope.row)">编辑</el-button>
+        <el-button type="primary" link :icon="Delete" @click="deleteAccount(scope.row)">删除</el-button>
+        <el-button type="primary" link :icon="View" @click="openDrawer('新建下级分类', scope.row)">新建下级分类</el-button>
+      </template>
+    </ProTable>
+    <UserDrawer ref="drawerRef" />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<script setup lang="tsx" name="useProTable">
+import { User } from "@/api/interface";
+import ProTable from "@/components/ProTable/index.vue";
+import UserDrawer from "@/views/proTable/components/UserDrawer.vue";
+import { ProTableInstance, ColumnProps } from "@/components/ProTable/interface";
+import { getUserList, deleteUser, editUser, addUser } from "@/api/modules/user";
+import { useHandleData } from "@/hooks/useHandleData";
+const proTable = ref<ProTableInstance>();
+const initParam = reactive({ type: 1 });
+
+const getTableList = (params: any) => {
+  let newParams = JSON.parse(JSON.stringify(params));
+  newParams.createTime && (newParams.startTime = newParams.createTime[0]);
+  newParams.createTime && (newParams.endTime = newParams.createTime[1]);
+  delete newParams.createTime;
+  return getUserList(newParams);
+};
+
+// 表格配置项
+const columns: ColumnProps<User.ResUserList>[] = [
+  { type: "selection", fixed: "left", width: 80 },
+  { prop: "operation", label: "操作", fixed: "left", width: 330 },
+  {
+    prop: "username",
+    label: "门店编码",
+    search: { el: "input" },
+    render: scope => {
+      return <span>{scope.row.gender}</span>;
+    }
+  },
+  { prop: "idCard", label: "门店名称", search: { el: "input" } }
+];
+
+// 打开 drawer(新增、查看、编辑)
+const drawerRef = ref<InstanceType<typeof UserDrawer> | null>(null);
+const openDrawer = (title: string, row: Partial<User.ResUserList> = {}) => {
+  const params = {
+    title,
+    isView: title === "查看",
+    row: { ...row },
+    api: title === "新增" ? addUser : title === "编辑" ? editUser : undefined,
+    getTableList: proTable.value?.getTableList
+  };
+  drawerRef.value?.acceptParams(params);
+};
+
+// 删除用户信息
+const deleteAccount = async (params: User.ResUserList) => {
+  await useHandleData(deleteUser, { id: [params.id] }, `删除【${params.username}】用户`);
+  proTable.value?.getTableList();
+};
+</script>
