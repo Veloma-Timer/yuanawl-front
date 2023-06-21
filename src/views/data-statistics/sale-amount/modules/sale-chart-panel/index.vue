14b2664f9997aa6cb433591cb0b8863c1e344dce
liuwei <lvyx620@163.com>
2023-06-21 18:30:37 +0800

    数据统计

--- src/views/data-statistics/sale-amount/modules/sale-chart-panel/index.vue
+++ src/views/data-statistics/sale-amount/modules/sale-chart-panel/index.vue
@@ -2,10 +2,10 @@
   <div class="wrap">
     <div class="left">
       <div class="content">
-        <Header :title="`${currentTimeSelect}销售`" class="header"></Header>
+        {{ currentTimeSelect }}
         <el-radio-group v-model="currentTimeSelect" size="large" @change="changeSelectDate" class="date-radio">
-          <template v-for="(item, index) in tabDateList" :key="index">
-            <el-radio-button :label="item.title" />
+          <template v-for="item in tabDateList" :key="item">
+            <el-radio-button :label="item" />
           </template>
         </el-radio-group>
         <div ref="echartsRef" class="sale-echarts"></div>
@@ -18,7 +18,7 @@
         </template>
       </el-radio-group>
       <div class="sale-content">
-        <div class="item" v-for="(item, index) in saleData" :key="index">
+        <div class="item" v-for="(item, index) in saleData" :key="index" @click="switchChart(item)">
           <table>
             <tr class="top">
               {{
@@ -38,69 +38,46 @@
 </template>
 
 <script setup lang="ts">
-import Header from "@/components/Header/index.vue";
 import { ref } from "vue";
 import * as echarts from "echarts";
 import { useEcharts } from "@/hooks/useEcharts";
 import { getAllBranch } from "@/api/modules/set";
 import { todaySales } from "@/api/modules/order";
+import { TIME_RANGE } from "@/public/option";
+
 const emit = defineEmits(["change-id"]);
 const echartsRef = ref<HTMLElement>();
+// 时间范围选择
+const currentTimeSelect = ref<string>("本日");
+const legendName = ref(`${currentTimeSelect.value}销售总额`);
 
 const saleData: any = ref([]);
 const getTodaySales = async (branchId: number, date: number) => {
   const {
-    data: {
-      salesMap: { current, preCurrent },
-      salesPrice,
-      arpa,
-      paidOrders,
-      buyNumber
-    }
+    data: { salesPrice, arpa, paidOrders, buyNumber }
   } = await todaySales(branchId, date);
-  const toDayX = current.map((item: any) => {
-    return item.name;
-  });
-  const toDayY = current.map((item: any) => {
-    return item.value;
-  });
-  const yesterdayY = preCurrent.map((item: any) => {
-    return item.value;
-  });
   saleData.value = [
     {
-      title: "今日销售金额",
+      title: `${currentTimeSelect.value}销售总额`,
       value: `￥${salesPrice}`
     },
     {
-      title: "客单价",
+      title: `${currentTimeSelect.value}订单总量`,
       value: `￥${arpa}`
     },
     {
-      title: "支付订单数",
+      title: `${currentTimeSelect.value}商品加价率`,
       value: paidOrders
     },
     {
-      title: "下单用户数",
+      title: "出售渠道个数",
       value: buyNumber
     }
   ];
-  initEcharts(toDayX, toDayY, yesterdayY);
+  // initEcharts(xData, yData1, yData2);
 };
 
-// 时间范围选择
-const currentTimeSelect = ref<string>("本日");
-const tabDateList = ref([
-  {
-    title: "本日"
-  },
-  {
-    title: "本周"
-  },
-  {
-    title: "本月"
-  }
-]);
+const tabDateList = ref(["本日", "本周", "本月"]);
 
 // 门店数据获取
 type BranchObj = { branchName: string; id: number };
@@ -145,6 +122,7 @@ async function changeCityDate(e: any) {
 
 // 日期范围切换
 function changeSelectDate(e: any) {
+  console.log(111, e);
   currentTimeSelect.value = e;
 }
 
@@ -169,9 +147,17 @@ watch(
   }
 );
 
-function initEcharts(toDayX: any, toDayY: any, yesterdayY: any) {
+function initEcharts(xData: any, yData: any, legendName: string, title: string) {
   let myChart: echarts.ECharts = echarts.init(echartsRef.value as HTMLElement);
   let option: echarts.EChartsOption = {
+    title: {
+      text: title,
+      top: 10,
+      textStyle: {
+        fontWeight: 500,
+        fontSize: 14
+      }
+    },
     tooltip: {
       trigger: "axis",
       axisPointer: {
@@ -182,7 +168,7 @@ function initEcharts(toDayX: any, toDayY: any, yesterdayY: any) {
       }
     },
     legend: {
-      data: ["今日", "昨日"],
+      data: [legendName],
       textStyle: {
         color: "#a1a1a1"
       },
@@ -197,8 +183,7 @@ function initEcharts(toDayX: any, toDayY: any, yesterdayY: any) {
     xAxis: [
       {
         type: "category",
-        boundaryGap: false,
-        data: toDayX,
+        data: xData,
         axisLabel: {
           color: "#a1a1a1"
         }
@@ -214,39 +199,38 @@ function initEcharts(toDayX: any, toDayY: any, yesterdayY: any) {
     ],
     series: [
       {
-        name: "今日",
-        type: "line",
-        // stack: "Total",
-        lineStyle: {
-          color: "#63ECFF"
-        },
-        areaStyle: {
-          color: "#63ECFF"
-        },
+        name: legendName,
+        type: "bar",
         emphasis: {
           focus: "series"
         },
-        data: toDayY
-      },
-      {
-        name: "昨日",
-        type: "line",
-        // stack: "Total",
-        lineStyle: {
-          color: "#2ED7FF"
-        },
-        areaStyle: {
-          color: "#2ED7FF"
-        },
-        emphasis: {
-          focus: "series"
-        },
-        data: yesterdayY
+        data: yData
       }
     ]
   };
   useEcharts(myChart, option);
+  const chartObserver = new ResizeObserver(() => {
+    myChart.resize();
+  });
+  chartObserver.observe(echartsRef.value as HTMLElement);
 }
+
+function switchChart(item: any) {
+  console.log("item", { ...item });
+  const xData = ["张三", "张三", "张三", "张三", "张三", "张三"];
+  const yData = [20, 30, 40, 50, 60, 70];
+  if ([`${currentTimeSelect.value}销售总额`, `${currentTimeSelect.value}商品加价率`].includes(item.title)) {
+    legendName.value = "销售";
+  } else if ([`${currentTimeSelect.value}订单总量`].includes(item.title)) {
+    legendName.value = "订单";
+  } else if (["出售渠道个数"].includes(item.title)) {
+    legendName.value = "渠道";
+  }
+  initEcharts(xData, yData, legendName.value, item.title);
+}
+onMounted(() => {
+  switchChart({ title: `${currentTimeSelect.value}销售总额` });
+});
 </script>
 
 <style scoped lang="scss">
