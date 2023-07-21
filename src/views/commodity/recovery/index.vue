678a66c2ab519f354f7de6a1c45384f2002f040a
167354 <49268882+Veloma-Timer@users.noreply.github.com>
2023-07-21 18:36:42 +0800

    backup

--- src/views/commodity/recovery/index.vue
+++ src/views/commodity/recovery/index.vue
@@ -101,12 +101,16 @@ const dataCallback = (data: any) => {
 
 // 如果你想在请求之前对当前请求参数做一些操作，可以自定义如下函数：params 为当前所有的请求参数（包括分页），最后返回请求列表接口
 // 默认不做操作就直接在 ProTable 组件上绑定	:requestApi="getUserList"
-const getTableList = (params: any) => {
+const getTableList = async (params: any) => {
   let newParams = JSON.parse(JSON.stringify(params));
   newParams.createTime && (newParams.startTime = newParams.createTime[0]);
   newParams.createTime && (newParams.endTime = newParams.createTime[1]);
   delete newParams.createTime;
-  return getRecycleList(newParams);
+  const data = await getRecycleList(newParams);
+
+  console.log(data, "data");
+
+  return data;
 };
 const getFixed = (str: string) => {
   if (str) {
@@ -152,7 +156,8 @@ const columns: ColumnProps<Commodity.Recovery>[] = [
     width: 160,
     enum: getAllList,
     search: { el: "select" },
-    fieldNames: { label: "typeName", value: "id" }
+    fieldNames: { label: "typeName", value: "id" },
+    render: ({ row }) => row.accountTypeNames
   },
   {
     prop: "accountNumber",
