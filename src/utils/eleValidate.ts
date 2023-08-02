7fb3534ddd8fd665e0d59088743f655dcc7e163b
167354 <49268882+Veloma-Timer@users.noreply.github.com>
2023-08-02 17:37:59 +0800

    fix some bugs

--- src/utils/eleValidate.ts
+++ src/utils/eleValidate.ts
@@ -6,7 +6,7 @@ import { typeCode } from "@/api/modules/commodity";
  *  @rule 手机号
  */
 export function checkPhoneNumber(rule: any, value: any, callback: any) {
-  const regexp = /^(((13[0-9]{1})|(15[0-9]{1})|(16[0-9]{1})|(17[3-8]{1})|(18[0-9]{1})|(19[0-9]{1})|(14[5-7]{1}))+\d{8})$/;
+  const regexp = /^1[3-9]\d{9}$/;
   if (value === "") return callback("请输入手机号码");
   if (!regexp.test(value)) return callback(new Error("请输入正确的手机号码"));
   const params = {
