7e624b8b8f2f996b1828f1e2c2d98ae4714a70e6
167354 <49268882+Veloma-Timer@users.noreply.github.com>
2023-08-24 06:19:06 +0800

    添加更新链接

--- src/main.ts
+++ src/main.ts
@@ -43,4 +43,33 @@ Object.keys(Icons).forEach(key => {
   app.component(key, Icons[key as keyof typeof Icons]);
 });
 
+import { checkUpdate, installUpdate, onUpdaterEvent } from "@tauri-apps/api/updater";
+import { relaunch } from "@tauri-apps/api/process";
+
+const unlisten = await onUpdaterEvent(({ error, status }) => {
+  // This will log all updater events, including status updates and errors.
+  console.log("Updater event", error, status);
+});
+
+try {
+  const { shouldUpdate, manifest } = await checkUpdate();
+
+  if (shouldUpdate) {
+    // You could show a dialog asking the user if they want to install the update here.
+    console.log(`Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`);
+
+    // Install the update. This will also restart the app on Windows!
+    await installUpdate();
+
+    // On macOS and Linux you will need to restart the app manually.
+    // You could use this step to display another confirmation dialog.
+    await relaunch();
+  }
+} catch (error) {
+  console.error(error);
+}
+
+// you need to call unlisten if your handler goes out of scope, for example if the component is unmounted.
+unlisten();
+
 app.use(ElementPlus).use(directives).use(router).use(I18n).use(pinia).mount("#app");
