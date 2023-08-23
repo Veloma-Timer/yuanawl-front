4229027894bb91d7afaa24a5defa0bd2a5a9d480
167354 <49268882+Veloma-Timer@users.noreply.github.com>
2023-08-24 06:30:57 +0800

    修改自动更新实际

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,8 @@ import { useTheme } from "@/hooks/useTheme";
 import { ElConfigProvider } from "element-plus";
 import { LanguageType } from "./stores/interface";
 import { useGlobalStore } from "@/stores/modules/global";
+import { checkUpdate, installUpdate, onUpdaterEvent } from "@tauri-apps/api/updater";
+import { relaunch } from "@tauri-apps/api/process";
 
 import en from "element-plus/es/locale/lang/en";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
@@ -23,10 +25,36 @@ initTheme();
 
 // init language
 const i18n = useI18n();
-onMounted(() => {
+onMounted(async () => {
   const language = globalStore.language ?? getBrowserLang();
   i18n.locale.value = language;
   globalStore.setGlobalState("language", language as LanguageType);
+
+  const unlisten = await onUpdaterEvent(({ error, status }) => {
+    // This will log all updater events, including status updates and errors.
+    console.log("Updater event", error, status);
+  });
+
+  try {
+    const { shouldUpdate, manifest } = await checkUpdate();
+
+    if (shouldUpdate) {
+      // You could show a dialog asking the user if they want to install the update here.
+      console.log(`Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`);
+
+      // Install the update. This will also restart the app on Windows!
+      await installUpdate();
+
+      // On macOS and Linux you will need to restart the app manually.
+      // You could use this step to display another confirmation dialog.
+      await relaunch();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  // you need to call unlisten if your handler goes out of scope, for example if the component is unmounted.
+  unlisten();
 });
 
 // element language
