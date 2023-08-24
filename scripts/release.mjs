f984d245023546884c173205acf6ab13c8d49b91
167354 <49268882+Veloma-Timer@users.noreply.github.com>
2023-08-24 11:15:09 +0800

    添加完整的配置

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -2,7 +2,6 @@ import { createRequire } from "module";
 import { execSync } from "child_process";
 import fs from "fs";
 
-import updatelog from "./updatelog.mjs";
 
 const require = createRequire(import.meta.url);
 
@@ -28,14 +27,11 @@ async function release() {
   const nextVersion = `${a}.${b}.${c}`;
   packageJson.version = nextVersion;
 
-  const nextTag = `v${nextVersion}`;
-  await updatelog(nextTag, "release");
-
   // 将新版本写入 package.json 文件
   fs.writeFileSync("./package.json", JSON.stringify(packageJson, null, 2));
 
   // 提交修改的文件，打 tag 标签（tag 标签是为了触发 github action 工作流）并推送到远程
-  execSync("git add ./package.json ./UPDATE_LOG.md");
+  // execSync("git add ./package.json ./UPDATE_LOG.md");
   execSync(`git commit -m "v${nextVersion}"`);
   execSync(`git tag -a v${nextVersion} -m "v${nextVersion}"`);
   execSync(`git push`);
